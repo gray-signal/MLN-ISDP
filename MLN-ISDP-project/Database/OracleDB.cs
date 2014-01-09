546953ad9ebf2c8e25b08ad3c3552d95478afcff
Gray Moriarty <graeme.mcb@gmail.com>
2014-01-09 13:21:26 -0400

    started project and typed a few lines of db crap
    let's get this shit started, oh gods.

---
+++ MLN-ISDP-project/Database/OracleDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using System.Data.OracleClient; // doesn't exist anymore? wtf?
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+
+namespace MLN_ISDP_project.Database
+{
+    class OracleDB : IDisposable
+    {
+        #region Member Level Variables
+
+        // could use System.Data.SqlDbType, but that's only if we were using MS SQL. need to find the proper dbtype
+        //  when i download Oracle's own provider libraries? placeholder for now
+        private readonly Dictionary<Type, System.Data.DbType> typeMap = new Dictionary<Type, System.Data.DbType>();
+
+
+        #endregion
+
+
+    }
+}
