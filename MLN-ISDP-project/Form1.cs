498a2175d0b2f329545d977f954cb5b7a9332147
Gray Moriarty <graeme.mcb@gmail.com>
2014-01-21 10:00:51 -0400

    some data binding for the grid view

--- MLN-ISDP-project/Form1.cs
+++ MLN-ISDP-project/Form1.cs
@@ -11,19 +11,36 @@ namespace MLN_ISDP_project
 {
     public partial class Form1 : Form
     {
+        private List<Part> selectedParts;
+
         public Form1()
         {
             InitializeComponent();
+            selectedParts = new List<Part>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Test.test();
+            //Test.test();
+
+            lstPartsQuery.DataSource = selectedParts;
         }
 
-        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        private void resetSource()
         {
+            lstPartsQuery.DataSource = null;
+            lstPartsQuery.DataSource = selectedParts;
+        }
 
+        private void btnAddParts_Click(object sender, EventArgs e)
+        {
+            string promptValue = PartSearchModal.ShowDialog("Please enter Part Number to add.", "Part Number");
+
+            Part addPart = new Database.PersistenceFactory().Create<Part>(promptValue);
+
+            selectedParts.Add(addPart);
+
+            this.resetSource();
         }
     }
 }
