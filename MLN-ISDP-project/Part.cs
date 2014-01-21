1c2fad5b92ea3a35e281e8b7b95eb27253de8f54
Gray Moriarty <graeme.mcb@gmail.com>
2014-01-21 09:58:51 -0400

    started work on part obj

---
+++ MLN-ISDP-project/Part.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MLN_ISDP_project
+{
+    class Part : MLN_ISDP_project.Database.Persistence
+    {
+        #region Properties
+
+        public string PartID { get; set; }
+        public string PartDescription { get; set; }
+        public double Section { get; set; }
+        public double ListPrice { get; set; }
+        public double CostPrice { get; set; }
+        public long QuantityOnHand { get; set; }
+        public long QuantityOnOrder { get; set; }
+        public long MinQuantity { get; set; }
+        public long Reserved { get; set; }
+
+        #endregion
+
+    }
+}
