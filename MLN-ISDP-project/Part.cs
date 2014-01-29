27bfc9df9284e01d93d787d2477addeb5d19644f
gray <graeme.mcb@gmail.com>
2014-01-29 11:20:57 -0400

    built more of the part object
    added constructors since we're no longer using the factory, and removed
    the ID property for the same reason. also changed some types around and
    made them nullable. then tidied up the part load() from database method.

--- MLN-ISDP-project/Part.cs
+++ MLN-ISDP-project/Part.cs
@@ -7,7 +7,7 @@ using System.Data;
 
 namespace MLN_ISDP_project
 {
-    class Part : MLN_ISDP_project.Database.Persistence
+    class Part /*: MLN_ISDP_project.Database.Persistence*/
     {
 
         public enum Indicator { INVOICE, ORDER, NONE }
@@ -16,14 +16,15 @@ namespace MLN_ISDP_project
 
         #region Properties
 
-        public new string id
-        {
-            get { return m_id; }
-            set
-            {
-                m_id = value;
-            }
-        }
+        
+        //public new string id
+        //{
+        //    get { return m_id; }
+        //    set
+        //    {
+        //        m_id = value;
+        //    }
+        //}
 
         public string PartID 
         {
@@ -36,20 +37,35 @@ namespace MLN_ISDP_project
 
 
         public string PartDescription { get; set; }
-        public double Section { get; set; }
-        public double ListPrice { get; set; }
-        public double CostPrice { get; set; }
-        public long QuantityOnHand { get; set; }
-        public long QuantityOnOrder { get; set; }
-        public long MinQuantity { get; set; }
-        public long Reserved { get; set; }
+        public double? Section { get; set; }
+        public decimal? ListPrice { get; set; }
+        public decimal? CostPrice { get; set; }
+        public double? QuantityOnHand { get; set; }
+        public double? QuantityOnOrder { get; set; }
+        public double? MinQuantity { get; set; }
+        public double? Reserved { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Part() : this("INVALID PART")
+        {
+            
+        }
+
+        public Part(string id)
+        {
+            m_id = id;
+        }
 
         #endregion
 
         #region Methods
 
-        internal void load(OracleDB in_db)
+        internal bool load(OracleDB in_db)
         {
+            bool loadComplete = false;
             if (!in_db.isConnected())
             {
                 in_db.connect();
@@ -57,8 +73,25 @@ namespace MLN_ISDP_project
 
             DataTable dt = in_db.readQuery("SELECT * FROM Parts WHERE PartID = '" + this.m_id + "'");
 
-            
-            Console.WriteLine("datatable: " + dt.ToString());
+            if (dt != null)
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    PartDescription = (string)dt.Rows[0]["PartDescription"];
+                    Section = (double?)dt.Rows[0].Field<double?>("Section") ?? 0;
+                    ListPrice = (decimal?)dt.Rows[0].Field<decimal?>("ListPrice");
+                    CostPrice = (decimal?)dt.Rows[0].Field<decimal?>("CostPrice");
+                    QuantityOnHand = (double?)dt.Rows[0].Field<double?>("QuantityOnHand") ?? 0;
+                    QuantityOnOrder = (double?)dt.Rows[0].Field<double?>("QuantityOnOrder") ?? 0;
+                    MinQuantity = (double?)dt.Rows[0].Field<double?>("MinQuantity") ?? 0;
+                    Reserved = dt.Rows[0].Field<double?>("Reserved") ?? 0;
+
+                    loadComplete = true;
+                }
+            }
+
+
+            return loadComplete;
         }
 
         #endregion
