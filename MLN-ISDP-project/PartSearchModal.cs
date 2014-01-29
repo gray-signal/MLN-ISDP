9646d6a866a9b460ff1c3aeeb5572419b085e025
gray <graeme.mcb@gmail.com>
2014-01-29 12:30:19 -0400

    changed label width, added a hack to fix buttons
    stupid hack to make cancel close out, but ok on empty field not close.

--- MLN-ISDP-project/PartSearchModal.cs
+++ MLN-ISDP-project/PartSearchModal.cs
@@ -15,12 +15,16 @@ namespace MLN_ISDP_project
             partSearch.Height = 200;
             partSearch.Text = caption;
 
-            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 200 };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 300 };
             TextBox inputBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
             Button confirm = new Button() { Text = "OK", Left = 150, Width = 100, Top = 70 };
             Button cancel = new Button() { Text = "Cancel", Left = 350, Width = 100, Top = 70 };
 
-            confirm.Click += (sender, e) => { partSearch.Close(); };
+            confirm.Click += (sender, e) => {
+                if (inputBox.Text == "")
+                    inputBox.Text = " "; // stupid hack to make cancel close out, but ok on empty field not close.
+                partSearch.Close(); 
+            };
 
             cancel.Click += (sender, e) => { inputBox.Text = null;
                                              partSearch.Close(); };
